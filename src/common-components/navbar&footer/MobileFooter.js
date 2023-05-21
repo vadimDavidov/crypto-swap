3492495176d6745fdff1876f6f8b3fd27678ead6
Vadim Davidov <vdvdavid21@gmail.com>
2023-05-21 14:12:18 +0300

    Change layout of mobile navbar

--- src/common-components/navbar&footer/MobileFooter.js
+++ src/common-components/navbar&footer/MobileFooter.js
@@ -3,102 +3,187 @@ import styles from './MobileFooter.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import { svgIcons } from '@/utils-data/svgIcons';
+import { createRef, useEffect, useRef, useState } from 'react';
 
 function MobileFooter() {
   const icons = svgIcons();
+  const clockwiseRef = createRef();
+  const questionRef = createRef();
+  const documentRef = createRef();
+  const answerRef = createRef();
+  const languageRef = createRef();
+  const ellipseRef = createRef();
+  const [hasFocus, setHasFocus] = useState(false);
+
+  // useEffect(() => {
+  //   if (document.hasFocus() && ref.current.contains(document.activeElement)) {
+  //     setHasFocus(true);
+  //   }
+  // }, [hasFocus]);
+
+  // useEffect(() => {
+  //   if (document.activeElement === clockwiseRef.current) {
+  //     setHasFocus(true);
+  //   }
+  //   if (document.activeElement !== clockwiseRef.current) {
+  //     setHasFocus(false);
+  //   }
+
+  //   if (document.activeElement === questionRef.current) {
+  //     setHasFocus(true);
+  //   }
+  //   if (document.activeElement !== questionRef.current) {
+  //     setHasFocus(false);
+  //   }
+
+  //   if (document.activeElement === documentRef.current) {
+  //     setHasFocus(true);
+  //   }
+  //   if (document.activeElement !== documentRef.current) {
+  //     setHasFocus(false);
+  //   }
+  //   if (document.activeElement === languageRef.current) {
+  //     setHasFocus(true);
+  //   }
+  //   if (document.activeElement !== languageRef.current) {
+  //     setHasFocus(false);
+  //   }
+  // }, [
+  //   hasFocus,
+  //   clockwiseRef,
+  //   questionRef,
+  //   documentRef,
+  //   answerRef,
+  //   languageRef,
+  // ]);
+
+  console.log(hasFocus);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.footer}>
         <nav>
           <div className={`${styles.innerBox} ${styles.clockwise}`}>
-            <Link href={'#'}>
+            <Link
+              onClick={() => clockwiseRef.current}
+              ref={useRef(clockwiseRef.current)}
+              href={'#'}
+            >
               <Image
                 className={styles.ellipse}
                 width={6}
                 height={6}
-                src="/icons/ellipse.svg"
-                // src={
-                //   FocusEvent ? '/icons/ellipse-focus.svg' : '/icons/ellipse.svg'
-                // }
+                // src="/icons/ellipse.svg"
+                src={
+                  hasFocus ? '/icons/ellipse-focus.svg' : '/icons/ellipse.svg'
+                }
                 alt="ellipse"
               />
               <Image
                 width={32}
                 height={32}
-                src="/icons/clockwise.svg"
-                // src={FocusEvent ? icons[0].focusPath : icons[0].path}
+                // src="/icons/clockwise.svg"
+                src={hasFocus ? icons[0].focusPath : icons[0].path}
                 alt="clockwise"
               />
             </Link>
           </div>
           <div className={`${styles.innerBox} ${styles.question}`}>
-            <Link href={'#'}>
+            <Link
+              onClick={() => questionRef.current}
+              ref={useRef(questionRef.current)}
+              href={'#'}
+            >
               <Image
                 className={styles.ellipse}
                 width={6}
                 height={6}
-                src="/icons/ellipse.svg"
+                // src="/icons/ellipse.svg"
+                src={
+                  hasFocus ? '/icons/ellipse-focus.svg' : '/icons/ellipse.svg'
+                }
                 alt="ellipse"
               />
               <Image
                 width={32}
                 height={32}
-                src="/icons/question.svg"
-                // src={FocusEvent ? icons[1].focusPath : icons[1].path}
+                // src="/icons/question.svg"
+                src={hasFocus ? icons[1].focusPath : icons[1].path}
                 alt="question"
               />
             </Link>
           </div>
           <div className={`${styles.innerBox} ${styles.document}`}>
-            <Link href={'#'}>
+            <Link
+              onClick={() => documentRef.current}
+              ref={useRef(documentRef.current)}
+              href={'#'}
+            >
               <Image
                 className={styles.ellipse}
                 width={6}
                 height={6}
-                src="/icons/ellipse.svg"
+                // src="/icons/ellipse.svg"
+                src={
+                  hasFocus ? '/icons/ellipse-focus.svg' : '/icons/ellipse.svg'
+                }
                 alt="ellipse"
               />
               <Image
                 width={32}
                 height={32}
-                src="/icons/document.svg"
-                // src={FocusEvent ? icons[2].focusPath : icons[2].path}
+                // src="/icons/document.svg"
+                src={hasFocus ? icons[2].focusPath : icons[2].path}
                 alt="document"
               />
             </Link>
           </div>
           <div className={`${styles.innerBox} ${styles.answer}`}>
-            <Link href={'#'}>
+            <Link
+              onClick={() => answerRef.current}
+              ref={useRef(answerRef.current)}
+              href={'#'}
+            >
               <Image
                 className={styles.ellipse}
                 width={6}
                 height={6}
-                src="/icons/ellipse.svg"
+                // src="/icons/ellipse.svg"
+                src={
+                  hasFocus ? '/icons/ellipse-focus.svg' : '/icons/ellipse.svg'
+                }
                 alt="ellipse"
               />
               <Image
                 width={32}
                 height={32}
-                src="/icons/answer.svg"
-                // src={FocusEvent ? icons[3].focusPath : icons[3].path}
+                // src="/icons/answer.svg"
+                src={hasFocus ? icons[3].focusPath : icons[3].path}
                 alt="answer"
               />
             </Link>
           </div>
           <div className={`${styles.innerBox} ${styles.language}`}>
-            <Link href={'#'}>
+            <Link
+              onClick={() => languageRef.current}
+              ref={useRef(languageRef.current)}
+              href={'#'}
+            >
               <Image
                 className={styles.ellipse}
                 width={6}
                 height={6}
-                src="/icons/ellipse.svg"
+                // src="/icons/ellipse.svg"
+                src={
+                  hasFocus ? '/icons/ellipse-focus.svg' : '/icons/ellipse.svg'
+                }
                 alt="ellipse"
               />
               <Image
                 width={32}
                 height={32}
-                src="/icons/lang-footer.svg"
-                // src={FocusEvent ? icons[4].focusPath : icons[4].path}
+                // src="/icons/lang-footer.svg"
+                src={hasFocus ? icons[4].focusPath : icons[4].path}
                 alt="language"
               />
             </Link>
